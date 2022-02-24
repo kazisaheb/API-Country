11e58694075558013b8502d7749bf9cb44f0e314
unknown <you@example.com>
2022-02-24 22:33:58 +0600

    Non

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const createItem = (res) => {
     div.innerHTML = `
      <h3> Country: ${data.name.common}</h3>
      <h4>Capital: ${data.capital}</h4>
-     <p>Currencies: ${key ? currencyObject[key].name : 'hello'}</p>
+     <p>Currencies: ${key ? currencyObject[key].name : 'NoN'}</p>
      <p>Region: ${data.region}</p>
      <p>Area: ${data.area} Sqr.KM</p>
      <p>Population: ${data.population}</p>
